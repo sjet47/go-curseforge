0ecbce8abd5b68ab7b61901e1388a22de302cf20
Aryan Sjet <sjet@asjet.dev>
2024-01-08 15:31:16 +0800

    feat(api): add files api

--- api/api.go
+++ api/api.go
@@ -2,11 +2,16 @@ package api
 
 import (
 	"net/http"
+
+	"github.com/ASjet/go-curseforge/api/files"
 )
 
 type API struct {
+	files.Files
 }
 
 func New(t http.RoundTripper) *API {
-	return &API{}
+	return &API{
+		Files: files.NewFilesAPI(t),
+	}
 }
---
+++ api/files/files.go
@@ -0,0 +1,114 @@
+package files
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+	"strconv"
+
+	"github.com/ASjet/go-curseforge/schema"
+)
+
+func NewFilesAPI(t http.RoundTripper) Files {
+	return func(modID string, o ...func(*FilesRequest)) (*http.Response, error) {
+		r := new(FilesRequest)
+		for _, f := range o {
+			f(r)
+		}
+		r.ModID = modID
+		return r.Do(r.ctx, t)
+	}
+}
+
+type Files func(modID string, o ...func(*FilesRequest)) (*http.Response, error)
+
+// https://docs.curseforge.com/#get-mod-files
+type FilesRequest struct {
+	ctx context.Context
+
+	ModID             string
+	GameVersion       *string
+	GameVersionTypeID *int
+	ModLoader         *schema.ModLoader
+	PageNum           int
+	PageSize          int
+}
+
+func (r *FilesRequest) Do(ctx context.Context, t http.RoundTripper) (*http.Response, error) {
+	var (
+		method = http.MethodGet
+		params = make(map[string]string)
+		path   = fmt.Sprintf("%s/v1/mods/%s/files", schema.BaseUrl, r.ModID)
+	)
+
+	if r.GameVersion != nil {
+		params["gameVersion"] = *r.GameVersion
+	}
+	if r.GameVersionTypeID != nil {
+		params["gameVersionTypeId"] = strconv.Itoa(*r.GameVersionTypeID)
+	}
+	if r.ModLoader != nil {
+		params["modLoaderType"] = r.ModLoader.Param()
+	}
+	if r.PageNum > 0 {
+		params["index"] = strconv.Itoa(r.PageNum)
+	}
+	if r.PageSize > 0 {
+		params["pageSize"] = strconv.Itoa(r.PageSize)
+	}
+
+	req, err := http.NewRequest(method, path, nil)
+	if err != nil {
+		return nil, err
+	}
+
+	if ctx != nil {
+		req = req.WithContext(ctx)
+	}
+
+	if len(params) > 0 {
+		query := req.URL.Query()
+		for k, v := range params {
+			query.Add(k, v)
+		}
+		req.URL.RawQuery = query.Encode()
+	}
+
+	return t.RoundTrip(req)
+}
+
+func (r Files) WithContext(ctx context.Context) func(*FilesRequest) {
+	return func(o *FilesRequest) {
+		o.ctx = ctx
+	}
+}
+
+func (r Files) WithGameVersion(gameVersion string) func(*FilesRequest) {
+	return func(o *FilesRequest) {
+		o.GameVersion = &gameVersion
+	}
+}
+
+func (r Files) WithGameVersionTypeID(gameVersionTypeID int) func(*FilesRequest) {
+	return func(o *FilesRequest) {
+		o.GameVersionTypeID = &gameVersionTypeID
+	}
+}
+
+func (r Files) WithModLoader(modLoader schema.ModLoader) func(*FilesRequest) {
+	return func(o *FilesRequest) {
+		o.ModLoader = &modLoader
+	}
+}
+
+func (r Files) WithPageNum(pageNum int) func(*FilesRequest) {
+	return func(o *FilesRequest) {
+		o.PageNum = pageNum
+	}
+}
+
+func (r Files) WithPageSize(pageSize int) func(*FilesRequest) {
+	return func(o *FilesRequest) {
+		o.PageSize = pageSize
+	}
+}
