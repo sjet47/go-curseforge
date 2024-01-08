7315650961468267b1617891443d34d3a09a0362
Aryan Sjet <sjet@asjet.dev>
2024-01-08 16:17:23 +0800

    fix(transport): use Set instead Add header

--- transport.go
+++ transport.go
@@ -12,6 +12,7 @@ type transport struct {
 }
 
 func (t *transport) RoundTrip(req *http.Request) (*http.Response, error) {
-	req.Header.Add("x-api-key", t.apiKey)
+	req.Header.Set("Accept", "application/json")
+	req.Header.Set("x-api-key", t.apiKey)
 	return t.underlyingTransport.RoundTrip(req)
 }
