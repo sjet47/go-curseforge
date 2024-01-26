184fd06df51af3fd3650a87219428b121c6c8760
Aryan Sjet <sjet@asjet.dev>
2024-01-26 20:15:38 +0800

    test(api): reorder unit tests as define

--- api_test.go
+++ api_test.go
@@ -10,157 +10,6 @@ import (
 	"github.com/ASjet/go-curseforge/schema/enum"
 )
 
-func TestGetFiles(t *testing.T) {
-	cli := NewClient(apiKey)
-
-	rsp, err := cli.ModFiles(32274,
-		cli.ModFiles.WithGameVersion("1.19.2"),
-		cli.ModFiles.WithModLoader(enum.ModLoaderForge),
-		cli.ModFiles.WithIndex(0),
-		cli.ModFiles.WithPageSize(1),
-	)
-	if err != nil {
-		panic(err)
-	}
-
-	fmt.Printf("FileName:\t%s\nFileDate:\t%s\nDownloadURL:\t%s\n",
-		rsp.Data[0].FileName, rsp.Data[0].FileDate, rsp.Data[0].DownloadURL)
-}
-
-func TestGetModFileChangelog(t *testing.T) {
-	cli := NewClient(apiKey)
-	rsp, err := cli.ModFileChangelog(32274, 4759554)
-	if err != nil {
-		panic(err)
-	}
-	printJson(rsp)
-}
-
-func TestGetModFileUrl(t *testing.T) {
-	cli := NewClient(apiKey)
-	rsp, err := cli.ModFileUrl(32274, 4759554)
-	if err != nil {
-		panic(err)
-	}
-	printJson(rsp)
-}
-
-func TestGetModFile(t *testing.T) {
-	cli := NewClient(apiKey)
-	rsp, err := cli.ModFile(32274, 4759554)
-	if err != nil {
-		panic(err)
-	}
-	printJson(rsp)
-}
-
-func TestGetModFiles(t *testing.T) {
-	cli := NewClient(apiKey)
-	rsp, err := cli.ModFiles(32274)
-	if err != nil {
-		panic(err)
-	}
-	printJson(rsp)
-}
-
-func TestSearchMod(t *testing.T) {
-	cli := NewClient(apiKey)
-	rsp, err := cli.SearchMod(enum.MinecraftGameID,
-		cli.SearchMod.WithGameVersion("1.19.2"),
-		cli.SearchMod.WithModLoaderType(enum.ModLoaderForge),
-		cli.SearchMod.WithSearchFilter("JourneyMap"),
-		cli.SearchMod.WithSortField(enum.ModsSearchSortFieldPopularity),
-		cli.SearchMod.WithSortOrder(enum.SortOrderDescending),
-		cli.SearchMod.WithIndex(0),
-		cli.SearchMod.WithPageSize(1),
-	)
-	if err != nil {
-		panic(err)
-	}
-	fmt.Printf("ModID: %d\nName: %s\nSummary: %s\n",
-		rsp.Data[0].ID, rsp.Data[0].Name, rsp.Data[0].Summary)
-}
-
-func TestGetMod(t *testing.T) {
-	cli := NewClient(apiKey)
-	rsp, err := cli.Mod(32274)
-	if err != nil {
-		panic(err)
-	}
-	printJson(rsp)
-}
-
-func TestGetMods(t *testing.T) {
-	cli := NewClient(apiKey)
-	rsp, err := cli.Mods([]schema.ModID{32274})
-	if err != nil {
-		panic(err)
-	}
-	printJson(rsp)
-}
-
-func TestGetFeaturedMods(t *testing.T) {
-	cli := NewClient(apiKey)
-	rsp, err := cli.FeaturedMods(
-		enum.MinecraftGameID,
-		cli.FeaturedMods.WithExcludedModIDs(0),
-		cli.FeaturedMods.WithGameVersionTypeID(1),
-	)
-	if err != nil {
-		panic(err)
-	}
-	printJson(rsp)
-}
-
-func TestGetModDescription(t *testing.T) {
-	cli := NewClient(apiKey)
-	rsp, err := cli.ModDescription(32274,
-		cli.ModDescription.WithStrippedContent(true),
-	)
-	if err != nil {
-		panic(err)
-	}
-	printJson(rsp)
-}
-
-func TestGetMinecraftVersions(t *testing.T) {
-	cli := NewClient(apiKey)
-	rsp, err := cli.MinecraftVersions()
-	if err != nil {
-		panic(err)
-	}
-	printJson(rsp)
-}
-
-func TestGetMinecraftVersion(t *testing.T) {
-	cli := NewClient(apiKey)
-	rsp, err := cli.MinecraftVersion("1.19.2")
-	if err != nil {
-		panic(err)
-	}
-	printJson(rsp)
-}
-
-func TestGetModLoaders(t *testing.T) {
-	cli := NewClient(apiKey)
-	rsp, err := cli.ModLoaders(
-		cli.ModLoaders.WithGameVersion("1.19.2"),
-	)
-	if err != nil {
-		panic(err)
-	}
-	printJson(rsp)
-}
-
-func TestGetModLoader(t *testing.T) {
-	cli := NewClient(apiKey)
-	rsp, err := cli.ModLoader("forge-43.3.7")
-	if err != nil {
-		panic(err)
-	}
-	printJson(rsp)
-}
-
 func TestGames(t *testing.T) {
 	cli := NewClient(apiKey)
 	rsp, err := cli.Games(
@@ -218,6 +67,119 @@ func TestCategories(t *testing.T) {
 	printJson(rsp)
 }
 
+func TestSearchMod(t *testing.T) {
+	cli := NewClient(apiKey)
+	rsp, err := cli.SearchMod(enum.MinecraftGameID,
+		cli.SearchMod.WithGameVersion("1.19.2"),
+		cli.SearchMod.WithModLoaderType(enum.ModLoaderForge),
+		cli.SearchMod.WithSearchFilter("JourneyMap"),
+		cli.SearchMod.WithSortField(enum.ModsSearchSortFieldPopularity),
+		cli.SearchMod.WithSortOrder(enum.SortOrderDescending),
+		cli.SearchMod.WithIndex(0),
+		cli.SearchMod.WithPageSize(1),
+	)
+	if err != nil {
+		panic(err)
+	}
+	fmt.Printf("ModID: %d\nName: %s\nSummary: %s\n",
+		rsp.Data[0].ID, rsp.Data[0].Name, rsp.Data[0].Summary)
+}
+
+func TestMod(t *testing.T) {
+	cli := NewClient(apiKey)
+	rsp, err := cli.Mod(32274)
+	if err != nil {
+		panic(err)
+	}
+	printJson(rsp)
+}
+
+func TestMods(t *testing.T) {
+	cli := NewClient(apiKey)
+	rsp, err := cli.Mods([]schema.ModID{32274})
+	if err != nil {
+		panic(err)
+	}
+	printJson(rsp)
+}
+
+func TestFeaturedMods(t *testing.T) {
+	cli := NewClient(apiKey)
+	rsp, err := cli.FeaturedMods(
+		enum.MinecraftGameID,
+		cli.FeaturedMods.WithExcludedModIDs(0),
+		cli.FeaturedMods.WithGameVersionTypeID(1),
+	)
+	if err != nil {
+		panic(err)
+	}
+	printJson(rsp)
+}
+
+func TestModDescription(t *testing.T) {
+	cli := NewClient(apiKey)
+	rsp, err := cli.ModDescription(32274,
+		cli.ModDescription.WithStrippedContent(true),
+	)
+	if err != nil {
+		panic(err)
+	}
+	printJson(rsp)
+}
+
+func TestModFile(t *testing.T) {
+	cli := NewClient(apiKey)
+	rsp, err := cli.ModFile(32274, 4759554)
+	if err != nil {
+		panic(err)
+	}
+	printJson(rsp)
+}
+
+func TestModFiles(t *testing.T) {
+	cli := NewClient(apiKey)
+
+	rsp, err := cli.ModFiles(32274,
+		cli.ModFiles.WithGameVersion("1.19.2"),
+		cli.ModFiles.WithModLoader(enum.ModLoaderForge),
+		cli.ModFiles.WithIndex(0),
+		cli.ModFiles.WithPageSize(1),
+	)
+	if err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("FileName:\t%s\nFileDate:\t%s\nDownloadURL:\t%s\n",
+		rsp.Data[0].FileName, rsp.Data[0].FileDate, rsp.Data[0].DownloadURL)
+}
+
+func TestFiles(t *testing.T) {
+	cli := NewClient(apiKey)
+	rsp, err := cli.Files([]schema.FileID{32274})
+	if err != nil {
+		panic(err)
+	}
+	printJson(rsp)
+}
+
+func TestModFileChangelog(t *testing.T) {
+	cli := NewClient(apiKey)
+	rsp, err := cli.ModFileChangelog(32274, 4759554)
+	if err != nil {
+		panic(err)
+	}
+	printJson(rsp)
+}
+
+func TestModFileUrl(t *testing.T) {
+	cli := NewClient(apiKey)
+	rsp, err := cli.ModFileUrl(32274, 4759554)
+	if err != nil {
+		panic(err)
+	}
+	printJson(rsp)
+}
+
 func TestFingerprintMatchesByGame(t *testing.T) {
 	cli := NewClient(apiKey)
 	rsp, err := cli.FingerprintMatchesByGame(enum.MinecraftGameID,
@@ -259,6 +221,44 @@ func TestFingerprintFuzzyMatches(t *testing.T) {
 			Fingerprints: []schema.Fingerprint{1},
 		}),
 	)
+	if err != nil {
+		panic(err)
+	}
+	printJson(rsp)
+}
+
+func TestMinecraftVersions(t *testing.T) {
+	cli := NewClient(apiKey)
+	rsp, err := cli.MinecraftVersions()
+	if err != nil {
+		panic(err)
+	}
+	printJson(rsp)
+}
+
+func TestMinecraftVersion(t *testing.T) {
+	cli := NewClient(apiKey)
+	rsp, err := cli.MinecraftVersion("1.19.2")
+	if err != nil {
+		panic(err)
+	}
+	printJson(rsp)
+}
+
+func TestModLoaders(t *testing.T) {
+	cli := NewClient(apiKey)
+	rsp, err := cli.ModLoaders(
+		cli.ModLoaders.WithGameVersion("1.19.2"),
+	)
+	if err != nil {
+		panic(err)
+	}
+	printJson(rsp)
+}
+
+func TestModLoader(t *testing.T) {
+	cli := NewClient(apiKey)
+	rsp, err := cli.ModLoader("forge-43.3.7")
 	if err != nil {
 		panic(err)
 	}
